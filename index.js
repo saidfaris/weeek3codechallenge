44ed84fa705541819e1b6658492cf52e53233b0b
sidee <said.issack@student.moringaschool.com>
2023-07-07 10:36:10 +0300

    redone  the assighnment

--- index.js
+++ index.js
@@ -1,72 +1,89 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch('films.json')
-  .then(response => response.json())
-  .then(data => {
-    const films = data.films;
-    console.log(films); 
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+const placeholderSection = document.querySelector('.placeholder');
+const movieList = document.querySelector('.movie-list');
+let movies;
+let currentMovie;
 
-  function movieList(data) {
-    data.forEach((movie) => {
-      const markUp = document.createElement('li')
-      markUp.classList.add(`listing${movie.id}`)
-      markUp.classList.add(`hello`)
-      markUp.innerHTML = movie.title
+const movieTitle = document.createElement('h2');
+const movieRuntime = document.createElement('p');
+const movieCapacity = document.createElement('p');
+const showtime = document.createElement('p');
+const ticketsAvailable = document.createElement('p');
+const movieDescription = document.createElement('p');
+const moviePoster = document.createElement('img');
 
-      document.querySelector("ul").append(markUp)
+const buyTicket = document.createElement('button');
 
-      // display first movie details
-      displayMovie(movie);
+const moviesUl = document.createElement('ul');
 
-      // display movie details on click of the movie titles
-      markUp.addEventListener("click", function () {
-        displayMovie(movie);
-      });
-    });
-  }
-
-  function displayMovie(movie) {
-    const movieName = document.getElementById("name");
-    const movieImage = document.getElementById("poster");
-    const description = document.getElementById("description");
-    const runTime = document.getElementById("runtime");
-    const showTime = document.getElementById("showtime");
-    const capacity = document.getElementById("capacity");
-    const sold = document.getElementById("sold");
-    const available = document.getElementById("available");
-    let availableTickets = movie.capacity - movie.tickets_sold;
-
-    movieName.innerHTML = movie.title;
-    movieImage.src = movie.poster;
-    description.innerHTML = movie.description;
-    runTime.innerHTML = movie.runtime + " min";
-    showTime.innerHTML = movie.showtime;
-    capacity.innerHTML = movie.capacity + " seats";
-    sold.innerHTML = movie.tickets_sold + " tickets";
-    available.innerHTML = availableTickets + " tickets";
-
-    husna(movie);
-  }
-
-  function husna(movie){
-    const alwy = document.getElementById("ticket");
-    const show = document.getElementById("show")
-    const run = document.getElementById("run");
-    const fat = document.getElementById("fat");
-    let remaining = (movie.capacity - movie.tickets_sold) - 1;
-
-    alwy.innerHTML = "Movie: " + movie.title;
-    show.innerHTML = "Showtime: " + movie.showtime;
-    run.innerHTML = "Runtime: " + movie.runtime + "min";
-    fat.innerHTML = remaining + " more tickets available you can purchase them on home page";
-  }
+buyTicket.addEventListener('click', () => {
+	reduceTicketNumbers(currentMovie);
 });
-$(document).ready(function(){
-  $("#btn").click(function(){
-        $(".details").hide();
-        $(".koz").show();
-  });
-});
+
+movieList.appendChild(moviesUl);
+
+const allMoviesUrl = 'https://api.npoint.io/1b4738002482f1452f54/films';
+
+document.addEventListener('DOMContentLoaded', fetchPlaceholderMovie);
+document.addEventListener('DOMContentLoaded', renderMoviesList);
+
+function fetchPlaceholderMovie() {
+	fetch(allMoviesUrl)
+		.then((res) => res.json())
+		.then((data) => {
+			currentMovie = data[0];
+			renderPlaceholderMovie(currentMovie);
+		});
+}
+
+function renderPlaceholderMovie(movie) {
+	const availableTickets = movie.capacity - movie.tickets_sold;
+	movieTitle.textContent = movie.title;
+	movieRuntime.textContent = `Runtime: ${movie.runtime} minutes`;
+	movieCapacity.textContent = `Capacity: ${movie.capacity}`;
+	showtime.textContent = `Showtimes: ${movie.showtime}`;
+	ticketsAvailable.textContent =
+		availableTickets > 0
+			? `Tickets Available: ${availableTickets}`
+			: 'No Tickets Available';
+	movieDescription.textContent = movie.description;
+	moviePoster.src = movie.poster;
+	buyTicket.innerText = availableTickets > 0 ? 'BuyTicket' : 'Sold Out';
+
+	buyTicket.className = buyTicket.innerText === 'Sold Out' ? 'disabled' : '';
+
+	placeholderSection.appendChild(movieTitle);
+	placeholderSection.appendChild(moviePoster);
+	placeholderSection.appendChild(movieDescription);
+	placeholderSection.appendChild(movieRuntime);
+	placeholderSection.appendChild(movieCapacity);
+	placeholderSection.appendChild(showtime);
+	placeholderSection.appendChild(ticketsAvailable);
+	placeholderSection.appendChild(buyTicket);
+}
+
+function renderMoviesList() {
+	fetch(allMoviesUrl)
+		.then((res) => res.json())
+		.then((data) => {
+			movies = data;
+			renderMovies(movies);
+		});
+}
+
+function renderMovies(movies) {
+	movies.map((movie) => {
+		const li = document.createElement('li');
+		li.textContent = movie.title;
+		moviesUl.appendChild(li);
+
+		li.addEventListener('click', () => {
+			currentMovie = movie;
+			renderPlaceholderMovie(currentMovie);
+		});
+	});
+}
+
+function reduceTicketNumbers(movie) {
+	movie.tickets_sold++;
+	renderPlaceholderMovie(movie);
+}
